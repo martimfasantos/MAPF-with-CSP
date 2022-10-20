1d0b0fecb5dc4c1be103bfc9a576c85485f737bd
Tiburso <manuelmariamtc@gmail.com>
2022-10-20 19:32:09 +0100

    changed distance calc

--- proj.py
+++ proj.py
@@ -6,8 +6,8 @@ import subprocess
 # import nx
 
 UNSAT = b"UNSATISFIABLE"
+ERROR = b"ERROR"
 SOLVER = 'Chuffed'  # Chuffed / Gecode
-
 
 
 def main(graph, scen):
@@ -20,24 +20,24 @@ def main(graph, scen):
     INF = n_edges+1
 
     # calculate every min distance using BFS
-    min_d = calc_min_vertex_dist(n_vertices, adjs, goal_pos)
-
-    print(min_d)
+    min_d, makespan = calc_min_vertex_dist(
+        n_vertices, adjs, goal_pos, start_pos)
 
     # calculate min makespan using BFS
-    makespan = calc_min_makespan(start_pos, goal_pos, min_d)
+    # makespan = calc_min_makespan(start_pos, goal_pos, min_d)
 
     # make the timetable matrix
     # timetable = make_timetable(n_vertices, adjs)
 
-    data = {'n_vertices': n_vertices,
-            'n_edges': n_edges,
-            'adj': adjs,
-            'n_agents': n_agents,
-            'start': start_pos,
-            'goal': goal_pos,
-            'min_d': min_d
-            }
+    data = {
+        'n_vertices': n_vertices,
+        'n_edges': n_edges,
+        'adj': adjs,
+        'n_agents': n_agents,
+        'start': start_pos,
+        'goal': goal_pos,
+        'min_d': min_d
+    }
 
     # probably JUMP will be 1 after optimized (USAT is detected faster)
     global JUMP
@@ -45,27 +45,29 @@ def main(graph, scen):
     print("JUMP:")
     print(JUMP)
 
-    output = UNSAT
-    if n_agents > 12:
-        global SOLVER
-        SOLVER = 'Gecode'
+    # if n_agents > 12:
+    #     global SOLVER
+    #     # SOLVER = 'Gecode'
 
     # print(SOLVER)
-    while UNSAT in output and makespan < 1000:
-
-        output = check_solution(SOLVER, data, makespan)
+    output = UNSAT
+    # while UNSAT in output and makespan < 1000:
+    while (UNSAT in output or ERROR in output) and makespan < 1000:
         print("------")
         print(makespan)
         print("------")
+        output = check_solution(SOLVER, data, makespan)
 
         makespan += JUMP
 
-    output = check_lower_makespan(output, SOLVER, data, makespan - JUMP)
+    # output = check_lower_makespan(output, SOLVER, data, makespan - JUMP)
+    print(output)
     print_output(output)
 
 
 def print_output(output):
     output = "".join(chr(x) for x in output)
+
     output = output.split("\n")[1:-3]
     for i, o in enumerate(output):
         o = o[3:].split(", ")
@@ -151,20 +153,49 @@ def bfs(adjs, start, goal):
                     queue.append(adj)
 
 
-def calc_min_vertex_dist(n_vertices, adjs, goal_pos):
+def weighted_bfs(d, goal, adjs, makespan):
+    dist = 0
+    q = [goal]
+    aux_q = []
+
+    # bfs marosca com tagging
+    while q != []:
+        # checkar distancia dos adjacentes
+        v = q.pop()
+
+        # se nao tiver sido visitado
+        if d[goal-1][v-1] == -1:
+            aux_q += adjs[v-1]
+            d[goal-1][v-1] = dist
+
+        if q == []:
+            dist += 1
+            q = aux_q
+            aux_q = []
+
+    return max(makespan, dist)
+
+
+def calc_min_vertex_dist(n_vertices, adjs, goal_pos, start_pos):
     d = [[-1] * n_vertices for _ in range(1, n_vertices+1)]
 
-    for goal in goal_pos:
-        for vertex in range(1, n_vertices+1):
-            path = bfs(adjs, goal, vertex)
-            # path = nx.shortest_path()
-            if path != None:
-                path_length = len(path)
-            else:
-                path_length = INF
-            d[goal-1][vertex-1] = path_length
+    makespan = 2
+    for goal, start in zip(goal_pos, start_pos):
+        makespan = weighted_bfs(d, goal, adjs, makespan)
 
-    return d
+        # for vertex in range(1, n_vertices+1):
+        #     path = bfs(adjs, goal, vertex)
+        #     # path = nx.shortest_path()
+        #     if path != None:
+        #         path_length = len(path)
+
+        #         if vertex == start:
+        #             makespan = max(makespan, path_length)
+        #     else:
+        #         path_length = INF
+        #     d[goal-1][vertex-1] = path_length
+
+    return d, makespan
 
 
 def calc_min_makespan(start_pos, goal_pos, min_d):
